26a959b6b6da02f4fb1c3e6059906c97a9ef9d65
Filip <lord.vidojevic@gmail.com>
2020-01-13 12:38:27 +0100

    Box Surface with Validation

---
+++ BoxSurface/BoxSurface/Box.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxSurface
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return (2 * length * width) + (2 * length * height) + (2 * width * height);
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return (2 * length * height) + (2 * width * height);
+        }
+
+        public double GetVolume()
+        {
+            return length * width * height;
+        }
+    }
+}
---
+++ BoxSurface/BoxSurface/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxSurface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Length: ");
+                double inputLength = double.Parse(Console.ReadLine());
+                Console.WriteLine("Width: ");
+                double inputWidth = double.Parse(Console.ReadLine());
+                Console.WriteLine("Height: ");
+                double inputHeight = double.Parse(Console.ReadLine());
+
+                Box box = new Box(inputLength, inputWidth, inputHeight);
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea()}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea()}");
+                Console.WriteLine($"Volume - {box.GetVolume()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
