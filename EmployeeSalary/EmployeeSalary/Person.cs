0b1e346981bb83a1e03eef8a578d32897485f1c7
Filip <lord.vidojevic@gmail.com>
2020-01-15 17:18:14 +0100

    Initial Commit

---
+++ EmployeeSalary/EmployeeSalary/Person.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        public Person(string firstName, string lastName, int age, double salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot be less than 3 symbols.");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            set
+            {
+                if (value.Length < 5)
+                {
+                    throw new ArgumentException("Last name cannot be less than 5 symbols.");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("People under 18 years, cannot work.");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+
+            set
+            {
+                if (value < 350)
+                {
+                    throw new ArgumentException("Salary cannot be less than 350$");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} have {this.salary} monthly";
+        }
+
+        public void IncreaseSalary(double percent)
+        {
+            if (this.age > 30)
+            {
+                this.salary += this.salary * percent / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percent / 200;
+            }
+        }
+    }
+}
---
+++ EmployeeSalary/EmployeeSalary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            //U petlji kreiramo novu osobu. Problem: Resiti parametre unutar konstruktora, lines oznacava koliko puta prolazimo kroz petlju!!!
+            for (int i = 0; i < lines; i++)
+            {
+                var person = new Person("Sam", "Smith", 18, 350.5);
+                persons.Add(person);
+            }
+
+            //Upisati bonus u % za povecanje plate
+            var bonus = double.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.ReadLine();
+        }
+    }
+}
